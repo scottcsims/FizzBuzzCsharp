ef9141bbb2d834125b578ba175b97806af5baf73
Scott Sims <ssims98@hotmail.com>
2016-01-13 15:55:52 -0600

    FizzBuzz

---
+++ FizzBuzz/Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzDemo
+{
+    public class FizzBuzz
+    {
+        public void IterateTo100()
+        {
+            for (var x = 1; x <= 100; x++)
+            {
+                if (CheckMultipleOfFiveAndThree(x))
+                {
+                    Debug.WriteLine(x + "FizzBuzz");
+                }
+                else if (CheckMultipleOfThree(x))
+                {
+                    Debug.WriteLine(x + "Fiz");
+                }
+                else if (CheckMultipleOfFive(x))
+                {
+                    Debug.WriteLine(x + "Buzz");
+                }
+                else
+                {
+                    Debug.WriteLine(x);
+                }
+            }
+        }
+
+        public bool CheckMultipleOfThree(int checkThree)
+        {
+            return ((checkThree%3) == 0);
+        }
+
+        public bool CheckMultipleOfFive(int checkFive)
+        {
+            return ((checkFive%5) == 0);
+        }
+
+        public bool CheckMultipleOfFiveAndThree(int checkFiveAndThree)
+        {
+            return ((checkFiveAndThree%5) == 0) && ((checkFiveAndThree%3) == 0);
+        }
+    }
+}
---
+++ UnitTestFizzBuzz/UnitTest1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using FizzBuzzDemo;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestFizzBuzz
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        //"Write a program that prints the numbers from 1 to 100. 
+        //But for multiples of three print “Fizz” instead of the number and 
+        //for the multiples of five print “Buzz”. 
+        //For numbers which are multiples of both three and five print “FizzBuzz”."
+        [TestMethod]
+        public void TestFizzBuzzIterate()
+        {
+            var fizzbuzz = new FizzBuzz();
+            Assert.IsNotNull(fizzbuzz);
+            fizzbuzz.IterateTo100();
+        }
+
+        [TestMethod]
+        public void TestFizzBuzzMultiplesOfThreeTrue()
+        {
+            var fizzbuzz = new FizzBuzz();
+            Assert.IsTrue(fizzbuzz.CheckMultipleOfThree(9));
+        }
+
+        [TestMethod]
+        public void TestFizzBuzzMultiplesOfThreeFalse()
+        {
+            var fizzbuzz = new FizzBuzz();
+            Assert.IsFalse(fizzbuzz.CheckMultipleOfThree(10));
+        }
+        [TestMethod]
+        public void TestFizzBuzzMultiplesOfFiveTrue()
+        {
+            var fizzbuzz = new FizzBuzz();
+            Assert.IsTrue(fizzbuzz.CheckMultipleOfFive(55));
+        }
+
+        [TestMethod]
+        public void TestFizzBuzzMultiplesOfFiveFalse()
+        {
+            var fizzbuzz = new FizzBuzz();
+            Assert.IsFalse(fizzbuzz.CheckMultipleOfFive(8));
+        }
+        [TestMethod]
+        public void TestFizzBuzzMultiplesOfFiveAndThreeTrue()
+        {
+            var fizzbuzz = new FizzBuzz();
+            Assert.IsTrue(fizzbuzz.CheckMultipleOfFiveAndThree(30));
+        }
+
+        [TestMethod]
+        public void TestFizzBuzzMultiplesOfFiveAndThreeFalse()
+        {
+            var fizzbuzz = new FizzBuzz();
+            Assert.IsFalse(fizzbuzz.CheckMultipleOfFiveAndThree(8));
+        }
+    }
+}
